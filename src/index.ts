56599156b177517d28fdafff6c6b2aa60e491068
Ankur Seth <iankurseth@gmail.com>
2023-04-23 18:03:03 +0530

    Published html-color-names@0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 import masterJson from '../master.json';
 
-interface ColorJson {
+interface PaletteType {
+	"50": string,
+	"100": string,
+	"200": string,
+	"300": string,
+	"400": string,
+	"500": string,
+	"600": string,
+	"700": string,
+	"800": string,
+	"900": string,
+	"950": string
+}
+
+interface ColorJsonType {
 	name: string,
 	lowername: string,
 	hex: string,
-	palette: any
-};
+	palette: PaletteType
+}
 
-const palettes: any = {};
-masterJson.colors.forEach((color: ColorJson) => {
+const colors: ColorJsonType[] = masterJson.colors;
+
+export const palettes: any = {};
+colors.forEach(color => {
 	palettes[color.lowername] = color.palette
 });
 
-export default palettes;
+const hcn: any = {
+	palettes
+};
+
+export default hcn;
